01baccff218cd3f5e5e9a45c84251929da323238
angie <agladinop@gmail.com>
2022-05-24 20:20:27 -0500

    remove unused

--- src/views/layout/Layout.jsx
+++ src/views/layout/Layout.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { Menu } from "./components/menu/Menu";
+import { Menu } from "../../components/menu/Menu";
 
 import styles from "./layout.module.css";
 
 export const Layout = () => {
-  return (
-    <div className={styles.container}>
-      <div className={`${styles.menu_responsive} ${styles.menu}`}>
-        <Menu />
-      </div>
-      <div className={styles.outlet}>
-        <Outlet />
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <div className={`${styles.menu_responsive} ${styles.menu}`}>
+                <Menu />
+            </div>
+            <div className={styles.outlet}>
+                <Outlet />
+            </div>
+        </div>
+    );
 };
